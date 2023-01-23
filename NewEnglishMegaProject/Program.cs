30f6089167af5ccc3514948c03bb7cae3fc1c013
Mamuka1987 <juvenale86@gmail.com>
2023-01-23 17:58:22 +0400

    Initiate commit

---
+++ NewEnglishMegaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewEnglishMegaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
